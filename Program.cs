403646b35346418e6198767e760fe4b87b782451
Natalia P <nspant@yandex.ru>
2022-11-28 14:04:26 +0300

    add main loop

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿string [] array = new string[] {"hello", "2", "world", ":-)"};
 string[] result = new string [0];
 
+foreach (var item in array)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref result, result.Length + 1);
+    result [result.Length-1] = item;
+    }
+}
+
+
 
 
